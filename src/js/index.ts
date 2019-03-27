d187fec5573d94f5febb3532ce3b73c884abe255
unknown <fisnik@aslani.eu>
2019-03-27 13:47:32 +0100

    df

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,6 +7,36 @@ import axios,{
 }from "../../node_modules/axios/index"
 import { ICar } from "./icar";
 let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+let GetAllCarsButton:HTMLButtonElement = <HTMLButtonElement>document.getElementById("getallbutton");
+
+let addbutton:HTMLButtonElement = <HTMLButtonElement> document.getElementById("addbutton");
+GetAllCarsButton.addEventListener("click", showAllCars)
+addbutton.addEventListener("click",addcar);
+function  addcar() :void {
+    let addmodelelement : HTMLInputElement = < HTMLInputElement> document.getElementById("addmodel");
+    let addvendorelement : HTMLInputElement = <HTMLInputElement> document.getElementById("addvendor ");
+    let addpriceelement : HTMLInputElement = < HTMLInputElement> document.getElementById("addprice");
+    let Mymodel : string = addmodelelement.value;
+    let Myvendor : string = addvendorelement.value;
+    let Myprice : number = +addpriceelement.value;
+    
+    axios.post<ICar>("https://webapicar20190326034339.azurewebsites.net/api/cars",
+    {model:Mymodel,vendor:Myvendor,price:Myprice}) 
+    .then(function(Response :AxiosResponse):void
+    {
+        console.log("statuskoden er :"+ Response.status)
+    })
+    .catch(
+   
+        function (error : AxiosError) : void{
+            
+            console.log(error);
+        }
+    )
+    
+ 
+}
+
 
 function showAllCars():void{
 
@@ -17,13 +47,22 @@ function showAllCars():void{
    {
        console.log("er i then")
        console.log(response);
+
+       let result : string="<ol>"
+
+       response.data.forEach((car: ICar)=>{
+           result +="<li>"+"  "  +car.model+"  "   +car.vendor +"  " +car.id+"<li>"
+       });
+       result+= "</ol>"
+       ContentElement.innerHTML=result;
+
    
    })
 
    .catch(
    
     function (error : AxiosError) : void{
-        console.log("error  i Dara's numse ");
+        
         console.log(error);
     }
     )
@@ -35,4 +74,4 @@ function showAllCars():void{
 
 
 }
-showAllCars();
+// showAllCars();
